27d8e43257a36628520f8cca3f1729d6bf849c62
Luis Campano Goldar <lcampanogoldar@danielcastelao.org>
2021-11-24 14:27:50 +0100

    Último Commit

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -3,22 +3,30 @@ package com.dam1;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Proyecto para aprender a hacer un Singleton");
+
+        //intento instanciar un objeto con el constructor privado
+        // esto es un error
+        // Singleton miUnicaInstancia = new Singleton();
+
+        // creamos un objeto
+        Singleton miUnicaInstancia = Singleton.getInstance();
+        // rellenamos los atributos
+        miUnicaInstancia.setEdad(20);
+        miUnicaInstancia.setNombre("Pepe");
+
+        // creamos otro objeto Singleton
+        // como la única manera es con getInstance(), este método se encargará
+        // de devolvernos el objeto creado anteriormente
+        Singleton otraInstanciaSeraLaMisma = Singleton.getInstance();
+        // podemos cambiar la edad
+        // comprobar con el debug que los dos objetos:
+        // miUnicaInstancia y otraInstanciaSeraLaMisma son el mismo
+        otraInstanciaSeraLaMisma.setEdad(34);
 
 
-        public class Singleton(){}
-
-            private static final Singleton instance;
-
-            private Singleton() {}
-
-            public static Singleton getInstance() {
-                if (instance == null) {
-                    instance = new Singleton();
-                }
-                return instance;
-            }
-        }
-
+        // version 1.1 del programa con otra instancia
+        Singleton versionInstancia = Singleton.getInstance();
+        versionInstancia.setNombre("version 1.1");
     }
 }
